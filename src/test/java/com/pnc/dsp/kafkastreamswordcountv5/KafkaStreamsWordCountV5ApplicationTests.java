704f57dbe01b42d419cfbbbebb1c6f1966866f62
Luis Bajo <luis.bajo@hotmail.com>
2022-02-03 22:19:21 -0600

    First commit.

---
+++ src/test/java/com/pnc/dsp/kafkastreamswordcountv5/KafkaStreamsWordCountV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc.dsp.kafkastreamswordcountv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsWordCountV5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
